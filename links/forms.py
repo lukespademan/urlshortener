63cce1264354072ac331bdc95f32d2b682caee2d
root <root@lspade.xyz>
2018-04-10 12:48:46 +0000

    changed url input to url input

--- links/forms.py
+++ links/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class NewLink(forms.Form):
-    destination = forms.URLField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    destination = forms.URLField(widget=forms.URLInput(attrs={'class': 'form-control'}))
