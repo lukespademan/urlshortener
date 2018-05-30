730d457a2358c923a9ce83d7fa42c376d2b3bd43
lukespademan <info@lukespademan.com>
2018-05-30 16:15:28 +0100

    added recaptcha

--- links/forms.py
+++ links/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from snowpenguin.django.recaptcha2.fields import ReCaptchaField
+from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
 
 
 class NewLink(forms.Form):
     destination = forms.URLField(widget=forms.URLInput(attrs={'class': 'form-control'}))
+    captcha = ReCaptchaField(widget=ReCaptchaWidget())
